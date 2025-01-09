0b761cffb07e890a91e0050e6aacd74dfa88fa72
Rodrigo123LV <reimanisrodrigo@gmail.com>
2025-01-10 00:06:35 +0200

    Add files via upload
    Atveriet GitHub repozitoriju:
    
    Ieejiet GitHub repozitorijā, kur atrodas projekta fails. Saite var izskatīties, piemēram: https://github.com/jusu-lietotajvards/projekta-nosaukums. Kopējiet klonēšanas URL:
    
    Nospiediet zaļo pogu "Code" (vai "Kods"). Izvēlieties HTTPS vai SSH URL (parasti HTTPS, piemēram, https://github.com/...). Nokopējiet URL, nospiežot uz "Copy" ikonas. Atveriet Git programmu vai komandrindu:
    
    Ja izmantojat Windows: Meklējiet "Git Bash" vai atveriet parasto komandu logu (Command Prompt). Ja izmantojat Visual Studio, to var darīt arī caur "Team Explorer". Izpildiet klonēšanas komandu: Komandā ievadiet šādu tekstu un nospiediet Enter:
    
    bash Copy code git clone https://github.com/jusu-lietotajvards/projekta-nosaukums Pārbaudiet lejupielādēto mapi:
    
    Pēc klonēšanas izpildes datorā būs mape ar projekta failiem.
    
    ////////////////////////////////////////////////////////////////////////////
    
    Projekta atvēršana un izpilde Visual Studio vidē (Ja ir, jo manuprāt šādi būtu pat vieglāk bet nemācēšu teikt.) Atveriet Visual Studio:
    
    Ja Visual Studio nav instalēts, lejupielādējiet to no oficiālās mājaslapas. Izvēlieties "Community" versiju, kas ir bez maksas. Atveriet projektu:
    
    Visual Studio izvēlnē izvēlieties: File > Open > Project/Solution. Atveriet .sln failu, kas atrodas jūsu klonētajā mapē. Uzstādiet projekta atkarības (Dependencies):
    
    Ja projekts izmanto specifiskas NuGet pakotnes, tās automātiski tiks ielādētas, kad pirmoreiz izveidosiet projektu. Ja tas nenotiek, spiediet labo peles taustiņu uz projekta faila un izvēlieties Restore NuGet Packages. Palaidiet projektu:
    
    Nospiediet zaļo pogu "Start" vai taustiņu F5. Projekts tiks kompilēts un palaists.

---
+++ Form1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label problemLabel;
+        private TextBox answerTextBox;
+        private Button submitButton;
+        private Button resetButton;
+        private Label scoreLabel;
+        private Label timerLabel;
+        private Label levelLabel;  
+        private Timer timer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.problemLabel = new System.Windows.Forms.Label();
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+
+            this.problemLabel.AutoSize = true;
+            this.problemLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold);
+            this.problemLabel.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.problemLabel.Location = new System.Drawing.Point(126, 610);
+            this.problemLabel.Name = "problemLabel";
+            this.problemLabel.Size = new System.Drawing.Size(128, 45);
+            this.problemLabel.TabIndex = 0;
+            this.problemLabel.Text = "Solve: ";
+
+            this.answerTextBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.answerTextBox.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.answerTextBox.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.answerTextBox.Location = new System.Drawing.Point(69, 671);
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.Size = new System.Drawing.Size(250, 42);
+            this.answerTextBox.TabIndex = 1;
+            this.answerTextBox.TextChanged += new System.EventHandler(this.answerTextBox_TextChanged);
+            this.answerTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.answerTextBox_KeyDown);
+
+            this.submitButton.AutoSize = true;
+            this.submitButton.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.submitButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.submitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.submitButton.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.submitButton.ForeColor = System.Drawing.Color.White;
+            this.submitButton.Location = new System.Drawing.Point(333, 674);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(100, 39);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            this.submitButton.MouseEnter += new System.EventHandler(this.submitButton_MouseEnter);
+            this.submitButton.MouseLeave += new System.EventHandler(this.submitButton_MouseLeave);
+
+            this.resetButton.AutoSize = true;
+            this.resetButton.BackColor = System.Drawing.Color.IndianRed;
+            this.resetButton.FlatAppearance.BorderColor = System.Drawing.Color.DarkSlateGray;
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetButton.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.resetButton.ForeColor = System.Drawing.Color.White;
+            this.resetButton.Location = new System.Drawing.Point(443, 674);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(86, 39);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            this.resetButton.MouseEnter += new System.EventHandler(this.resetButton_MouseEnter);
+            this.resetButton.MouseLeave += new System.EventHandler(this.resetButton_MouseLeave);
+
+            this.scoreLabel.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.scoreLabel.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.scoreLabel.Location = new System.Drawing.Point(63, 734);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(193, 35);
+            this.scoreLabel.TabIndex = 4;
+            this.scoreLabel.Text = "Score: 0";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+
+            this.timerLabel.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.timerLabel.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.timerLabel.Location = new System.Drawing.Point(213, 734);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(200, 35);
+            this.timerLabel.TabIndex = 5;
+            this.timerLabel.Text = "Time Left: 30";
+            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            this.levelLabel.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.levelLabel.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.levelLabel.Location = new System.Drawing.Point(413, 734);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(120, 35);
+            this.levelLabel.TabIndex = 6;
+            this.levelLabel.Text = "Level: 1";
+            this.levelLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(600, 880);
+            this.Controls.Add(this.levelLabel);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.answerTextBox);
+            this.Controls.Add(this.problemLabel);
+            this.Name = "Form1";
+            this.Text = "Math Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void submitButton_MouseEnter(object sender, EventArgs e)
+        {
+            submitButton.BackColor = Color.DarkGreen;  
+            submitButton.ForeColor = Color.White;  
+        }
+
+        private void submitButton_MouseLeave(object sender, EventArgs e)
+        {
+            submitButton.BackColor = Color.MediumSeaGreen;  
+            submitButton.ForeColor = Color.White;  
+        }
+
+        private void resetButton_MouseEnter(object sender, EventArgs e)
+        {
+            resetButton.BackColor = Color.DarkRed;  
+        }
+
+        private void resetButton_MouseLeave(object sender, EventArgs e)
+        {
+            resetButton.BackColor = Color.IndianRed;  
+        }
+    }
+}
---
+++ Form1/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int score = 0;
+        private int timeLeft = 30; 
+        private string currentProblem;
+        private double correctAnswer;
+        private int level = 1; 
+        private int wrongAnswersInARow = 0; 
+
+        private Label fallingProblemLabel; 
+        private Timer fallingTimer; 
+        private int fallingSpeed = 1; 
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fallingTimer = new Timer();
+            fallingTimer.Interval = 50; 
+            fallingTimer.Tick += new EventHandler(FallingTimer_Tick);
+            StartNewProblem(); 
+        }
+
+        private void StartNewProblem()
+        {
+            timer.Stop(); 
+            fallingTimer.Stop(); 
+            if (fallingProblemLabel != null)
+            {
+                this.Controls.Remove(fallingProblemLabel); 
+            }
+
+            currentProblem = GenerateMathProblem();
+            correctAnswer = EvaluateMathProblem(currentProblem);
+
+            fallingProblemLabel = new Label();
+            fallingProblemLabel.Text = currentProblem;
+            fallingProblemLabel.Font = new Font("Arial", 16, FontStyle.Bold);
+            fallingProblemLabel.Size = new Size(150, 40); 
+            fallingProblemLabel.Location = new Point(random.Next(0, this.ClientSize.Width - 150), 0); 
+            fallingProblemLabel.ForeColor = Color.DarkSlateGray;
+            this.Controls.Add(fallingProblemLabel);
+
+            answerTextBox.Clear();
+            answerTextBox.Focus();
+            timeLeft = 30;
+            timerLabel.Text = $"Time Left: {timeLeft}";
+            levelLabel.Text = $"Level: {level}"; 
+            timer.Start(); 
+            fallingTimer.Start(); 
+        }
+
+        private string GenerateMathProblem()
+        {
+            int rangeAdditionSubtraction = level * 10; 
+            int rangeMultiplicationDivision = 1 + (level / 2); 
+
+            int num1, num2;
+            char[] operations;
+
+            if (level <= 3)
+            {
+                operations = new char[] { '+', '-' }; 
+            }
+            else if (level <= 6)
+            {
+                operations = new char[] { '+', '-', '*' }; 
+            }
+            else
+            {
+                operations = new char[] { '+', '-', '*', '/' }; 
+            }
+
+            char operation = operations[random.Next(operations.Length)];
+
+            if (operation == '+' || operation == '-')
+            {
+                num1 = random.Next(1, rangeAdditionSubtraction + 1);
+                num2 = random.Next(1, rangeAdditionSubtraction + 1);
+            }
+            else if (operation == '*')
+            {
+                num1 = random.Next(1, rangeMultiplicationDivision + 1);
+                num2 = random.Next(1, rangeMultiplicationDivision + 1);
+            }
+            else 
+            {
+                num1 = random.Next(1, rangeMultiplicationDivision + 1);
+                num2 = random.Next(1, rangeMultiplicationDivision + 1);
+                while (num2 == 0 || !IsFiniteDecimal((double)num1 / num2))
+                {
+                    num2 = random.Next(1, rangeMultiplicationDivision + 1);
+                }
+            }
+
+            string problem = $"{num1} {operation} {num2}";
+
+            for (int i = 0; i < level / 2; i++)
+            {
+                operation = operations[random.Next(operations.Length)];
+                int nextNum = random.Next(1, rangeAdditionSubtraction + 1);
+                problem = $"{problem} {operation} {nextNum}";
+
+                if (level >= 12 && random.Next(0, 2) == 0)
+                {
+                    problem = $"({problem})";
+                }
+            }
+
+            return problem;
+        }
+
+        private bool IsFiniteDecimal(double result)
+        {
+            return (result * 100) % 1 == 0;
+        }
+
+        private double EvaluateMathProblem(string problem)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                double result = Convert.ToDouble(table.Compute(problem, string.Empty));
+                return Math.Round(result, 2); 
+            }
+            catch
+            {
+                return 0; 
+            }
+        }
+
+        private void FallingTimer_Tick(object sender, EventArgs e)
+        {
+            if (fallingProblemLabel != null)
+            {
+                fallingProblemLabel.Top += fallingSpeed;
+
+                if (fallingProblemLabel.Bottom >= this.ClientSize.Height)
+                {
+                    fallingTimer.Stop();
+                    timer.Stop();
+                    MessageBox.Show($"You missed it! The correct answer was {correctAnswer}.", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    ResetButton_Click(sender, e);
+                }
+            }
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            ProcessAnswer();
+        }
+
+        private void ProcessAnswer()
+        {
+            if (double.TryParse(answerTextBox.Text, out double userAnswer))
+            {
+                timer.Stop();
+                fallingTimer.Stop();
+
+                if (Math.Abs(userAnswer - correctAnswer) < 0.01)
+                {
+                    score += 10;
+                    wrongAnswersInARow = 0;
+                    MessageBox.Show("Correct!", "Correct Answer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    score -= 5;
+                    wrongAnswersInARow++;
+                    MessageBox.Show($"Incorrect! The correct answer was {correctAnswer}", "Incorrect Answer", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    if (wrongAnswersInARow >= 3)
+                    {
+                        MessageBox.Show("You have answered incorrectly 3 times in a row. Game Over!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        ResetButton_Click(this, EventArgs.Empty);
+                        return;
+                    }
+                }
+
+                scoreLabel.Text = $"Score: {score}";
+                level = (score / 50) + 1;
+                levelLabel.Text = $"Level: {level}";
+
+                StartNewProblem();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            timerLabel.Text = $"Time Left: {timeLeft}";
+
+            if (timeLeft == 0)
+            {
+                timer.Stop();
+                fallingTimer.Stop();
+                MessageBox.Show("Time's up!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ProcessAnswer();
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            level = 1;
+            wrongAnswersInARow = 0;
+            scoreLabel.Text = $"Score: {score}";
+            levelLabel.Text = $"Level: {level}";
+            timeLeft = 30;
+            timerLabel.Text = $"Time Left: {timeLeft}";
+            timer.Stop();
+            fallingTimer.Stop();
+            if (fallingProblemLabel != null)
+            {
+                this.Controls.Remove(fallingProblemLabel);
+                fallingProblemLabel = null;
+            }
+            StartNewProblem();
+        }
+
+        private void answerTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SubmitButton_Click(sender, e);
+            }
+        }
+        private void answerTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!double.TryParse(answerTextBox.Text, out _))
+            {
+                answerTextBox.BackColor = Color.WhiteSmoke; 
+            }
+            else
+            {
+                answerTextBox.BackColor = Color.White; 
+            }
+        }
+    }
+}
